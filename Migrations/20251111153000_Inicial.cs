51033a14ee7f84475a10c8c070949c56f8dbfb46
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-11 15:11:48 -0300

    Create 20251111153000_Inicial.cs

---
+++ Migrations/20251111153000_Inicial.cs
@@ -0,0 +1,32 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrilhaApiDesafio.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tarefas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(nullable: false),
+                    Descricao = table.Column<string>(nullable: true),
+                    Data = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarefas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Tarefas");
+        }
+    }
+}
